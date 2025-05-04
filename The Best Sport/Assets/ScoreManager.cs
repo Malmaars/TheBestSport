1a842ef0631bf4bba467340ac601151d43b2f2f7
Simon <simondeb1505@gmail.com>
2025-05-04 22:10:47 +0200

    Added: Scoreboard

--- The Best Sport/Assets/ScoreManager.cs
+++ The Best Sport/Assets/ScoreManager.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
@@ -6,6 +7,9 @@ public class ScoreManager : MonoBehaviour
 
     //if the score goes to the negative of scoretowin, the right side wins, and if it goes to the positive of scoretowin, the left side wins
     public int score;
+
+    public RectTransform scoreBoardBackground;
+    public RectTransform scorePin;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,21 +20,31 @@ public class ScoreManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            AddScore(-1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            AddScore(1);
+        }
     }
 
-    void AddScore(bool _left, int points)
+    void AddScore(int points)
     {
-        if (_left)
-            score += points;
-        else 
-            score -= points;
-
+        score += points;
+        
         UpdateScoreBoard();
     }
 
     void UpdateScoreBoard()
     {
+        //place the score counter a given distance to the left or right
+
+        //take the width of the scoreboard and divide it by twice the required score
+        float oneStep = (scoreBoardBackground.sizeDelta.x / 2) / scoreToWin;
+
+        scorePin.anchoredPosition = new Vector2(oneStep * score, scorePin.anchoredPosition.y);
 
     }
 }
